3661be5d799dbbb782eabe291a4fe6eab69de52b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-09 16:02:12 +0900

    :new: テスト出来るように

---
+++ Assets/Scripts/SoramimiController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace HK.Soramimi
+{
+    public sealed class SoramimiController : MonoBehaviour
+    {
+        [SerializeField]
+        private string text;
+        
+        [SerializeField]
+        private Database database;
+
+        void Start()
+        {
+            Debug.Log(Soramimi.Translate(this.text, this.database));
+        }
+    }
+}
