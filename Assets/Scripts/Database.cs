9e743953dcc05112e33e2342f0579de02ff666e0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-09 13:51:04 +0900

    :new: とりあえず必要そうなファイルを追加

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace HK.SoramimiSystem
+{
+    [CreateAssetMenu()]
+    public sealed class Database : ScriptableObject
+    {
+        [SerializeField]
+        private List<Data> database;
+
+        public List<Data> Get
+        {
+            get { return this.database; }
+        }
+        
+        public class Data
+        {
+            [SerializeField]
+            private string key;
+
+            [SerializeField]
+            private string value;
+            
+            public string Key { get { return key; } }
+
+            public string Value { get { return value; } }
+        }
+    }
+}
---
+++ Assets/Scripts/Soramimi.cs
@@ -0,0 +1,10 @@
+﻿namespace HK.SoramimiSystem
+{
+    public sealed class Soramimi
+    {
+        public string Translate(string text, Database database)
+        {
+            return text;
+        }
+    }
+}
